54a1ffa8403bdb061dce5ba712fe37ef22e135ee
Kirill <kirill.artukhov@bk.ru>
2021-11-09 23:29:56 +0300

    7

---
+++ 1 semester/сдано/lab7 (2)/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string str;
+	int t, k;
+	cout << "Enter the groups of '0' and '1' without spaces:\n";
+
+	cin >> str;
+
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		k = i;
+
+		while (str[i] == str[i + 1])
+		{
+			i++;
+		}
+
+		if ((i - k) % 2)
+		{
+			for (t = k; t <= i; t++)
+			{
+				cout << str[t];
+			}
+			cout << " ";
+		}
+
+	}
+}
