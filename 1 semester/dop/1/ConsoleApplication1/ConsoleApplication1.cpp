3d148e59a535a4cc0cd185f14611cfccbf0e8982
Kirill <kirill.artukhov@bk.ru>
2021-12-13 23:14:45 +0300

    321

---
+++ 1 semester/dop/1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+double fun(double k)
+{
+    return pow(k, 3);
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    double a, b;
+    cout << "Введите два числа" << endl;
+    cin >> a >> b;
+
+    cout << fun(a) + fun(b) - fun(a + b);
+    
+}
+
--- 1 semester/сдано/lab4/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ 1 semester/сдано/lab4/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,82 +2,12 @@
 #include <iostream>
 using namespace std;
 
-
-double factorial(int k)
-{
-	if (k < 0) return 0;
-	if (k == 0) return 1;
-	else return k * factorial(k - 1);
-}
-
-
-double fun1(double x)
-{	
-	return  cos(x);	
-}
-
-
-double fun2(double x, double sum, double n)
-{
-	for (double k = 1; k <= n; k++)
-	{
-		sum += pow(-1, k) * pow(x, 2 * k) / factorial(2 * k);
-	}
-	return sum;
-}
-
-
-double fun3(double y, double sum)
-{
-	return fabs(y - sum);
-}
-
-
-void Out_Rez(int l, double y, double sum, double m)
-{
-	switch (l)
-	{
-		case 1:
-			cout << y << endl;
-			break;
-		
-		case 2:
-			cout << sum << endl;
-			break;
-		
-		case 3:
-			cout << fabs(y - sum) << endl;
-			break;
-		
-		default:
-			cout << "Error";
-			break;
-	}
-}
-
-
-void switchfun(int l)
-{
-	switch (l)
-	{
-		case 1:
-			cout << "Y(x)" << endl;
-			break;
-
-		case 2:
-			cout << "S(x)" << endl;
-			break;
-
-		case 3:
-			cout << "|Y(x) - S(x)|" << endl;
-			break;
-
-		default:
-			cout << "Error";
-			break;
-	}
-}	
-
+double factorial(int k);
+double fun1(double x);
+double fun2(double x, double sum, double n);
+double fun3(double y, double sum);
+void Out_Rez(int l, double y, double sum, double m);
+void switchfun(int l);
 
 int main()
 {
@@ -103,5 +33,79 @@ int main()
 	}	
 }
 
+double factorial(int k)
+{
+	if (k < 0) return 0;
+	if (k == 0) return 1;
+	else return k * factorial(k - 1);
+}
+
+
+double fun1(double x)
+{
+	return  cos(x);
+}
+
+
+double fun2(double x, double sum, double n)
+{
+	for (double k = 1; k <= n; k++)
+	{
+		sum += pow(-1, k) * pow(x, 2 * k) / factorial(2 * k);
+	}
+	return sum;
+}
+
+
+double fun3(double y, double sum)
+{
+	return fabs(y - sum);
+}
+
+
+void Out_Rez(int l, double y, double sum, double m)
+{
+	switch (l)
+	{
+	case 1:
+		cout << y << endl;
+		break;
+
+	case 2:
+		cout << sum << endl;
+		break;
+
+	case 3:
+		cout << fabs(y - sum) << endl;
+		break;
+
+	default:
+		cout << "Error";
+		break;
+	}
+}
+
+
+void switchfun(int l)
+{
+	switch (l)
+	{
+	case 1:
+		cout << "Y(x)" << endl;
+		break;
+
+	case 2:
+		cout << "S(x)" << endl;
+		break;
+
+	case 3:
+		cout << "|Y(x) - S(x)|" << endl;
+		break;
+
+	default:
+		cout << "Error";
+		break;
+	}
+}
 
 
